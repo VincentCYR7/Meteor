53c7656491e64e5c5f5dac04cb605a51cbcd1b8f
rimu09 <168046166+rimu09@users.noreply.github.com>
2024-06-27 13:32:23 +0900

    勝敗判定
    勝敗判定を作った

---
+++ Assets/Scripts/syouhai.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class syouhai : MonoBehaviour
+{
+    public Player player;
+    public Enemy[] enemies;
+
+    void Start()
+    {
+        // ゲームの初期化
+        InitializeGame();
+    }
+
+    void Update()
+    {
+        // ゲームの状態をチェック
+        CheckGameState();
+    }
+
+    void InitializeGame()
+    {
+        // プレイヤーを初期化
+        player = new Player();
+        player.health = 100;
+        player.isAlive = true;
+
+        // 敵を初期化
+        enemies = new Enemy[10];
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i] = new Enemy();
+            enemies[i].isAlive = true;
+        }
+    }
+
+    void CheckGameState()
+    {
+        bool allEnemiesDefeated = true;
+        foreach (Enemy enemy in enemies)
+        {
+            if (enemy.isAlive)
+            {
+                allEnemiesDefeated = false;
+                break;
+            }
+        }
+
+        if (allEnemiesDefeated)
+        {
+            Debug.Log("プレイヤーの勝利！敵を全滅させました。");
+        }
+        else if (!player.isAlive)
+        {
+            Debug.Log("プレイヤーの敗北！敵に摂食されました。");
+        }
+        else
+        {
+            Debug.Log("ゲーム続行中...");
+        }
+    }
+    [System.Serializable]
+    public class Player
+    {
+        public int health;
+        public bool isAlive;
+    }
+
+    [System.Serializable]
+    public class Enemy
+    {
+        public bool isAlive;
+    }
+}
+
